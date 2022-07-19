c6a6a54c86953df1525d814b1b98742add760f79
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-07-19 17:35:01 +0200

    chore: add projectionValidatorTest

--- src/DatasourceToolkit/Validations/ProjectionValidator.php
+++ src/DatasourceToolkit/Validations/ProjectionValidator.php
@@ -4,15 +4,15 @@ namespace ForestAdmin\AgentPHP\DatasourceToolkit\Validations;
 
 use Exception;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
-use Illuminate\Support\Collection as IlluminateCollection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\Projection\Projection;
 
 class ProjectionValidator
 {
     /**
      * @throws Exception
      */
-    public static function validate(Collection $collection, IlluminateCollection $projection): void
+    public static function validate(Collection $collection, Projection $projection): void
     {
-        $projection->each(fn ($field) => FieldValidator::validate($collection, $field));
+        collect($projection)->each(fn ($field) => FieldValidator::validate($collection, $field));
     }
 }
