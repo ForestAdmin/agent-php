acf3f86ba348bd57afa3bd6ef70d2806bbee53d7
Matt <matthv@gmail.com>
2022-07-22 16:55:24 +0200

    chore: add new SortFactory

---
+++ src/DatasourceToolkit/Components/Query/Sort/SortFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\Sort;
+
+use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\Sort;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Utils\Schema;
+
+class SortFactory
+{
+    public static function byPrimaryKeys(Collection $collection): Sort
+    {
+        return new Sort(
+            ...Schema::getPrimaryKeys()
+        );
+    }
+}
