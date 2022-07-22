7271ed22a4d2ee85331473dc2c1fc7ea583800c7
Matt <matthv@gmail.com>
2022-07-22 10:13:24 +0200

    feat(caller): add new method makeFromRequestData

--- src/DatasourceToolkit/Components/Caller.php
+++ src/DatasourceToolkit/Components/Caller.php
@@ -2,6 +2,8 @@
 
 namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components;
 
+use Illuminate\Support\Str;
+
 class Caller
 {
     public function __construct(
@@ -11,9 +13,23 @@ class Caller
         protected string $lastName,
         protected string $team,
         protected int $renderingId,
-        protected string $role,
         protected array $tags,
         protected string $timezone,
+        protected ?string $role = null,
     ) {
+    }
+
+    public static function makeFromRequestData(object $requestObjectData, $timezone): self
+    {
+        $data = (array) $requestObjectData;
+        $data['timezone'] = $timezone;
+        unset($data['exp']);
+
+        $attributes = [];
+        foreach ($data as $key => $value) {
+            $attributes[Str::camel($key)] = $value;
+        }
+
+        return (new static(...$attributes));
     }
 }
