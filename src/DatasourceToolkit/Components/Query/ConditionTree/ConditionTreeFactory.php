8a566e5e71c177811ebe65b85d4090f597c1a82d
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-07-26 14:05:02 +0200

    feat(ConditionTree):  add ConditionTreeFactory with tests

--- src/DatasourceToolkit/Components/Query/ConditionTree/ConditionTreeFactory.php
+++ src/DatasourceToolkit/Components/Query/ConditionTree/ConditionTreeFactory.php
@@ -2,18 +2,133 @@
 
 namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree;
 
+use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\ConditionTree;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\ConditionTreeBranch;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\ConditionTreeLeaf;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Utils\Schema as SchemaUtils;
+use Illuminate\Support\Collection as IlluminateCollection;
+
 class ConditionTreeFactory
 {
-    public function intersect() {
+    public static function matchRecords(Collection $collection, IlluminateCollection $records): ConditionTree
+    {
+        // TODO
+        //const ids = records.map(r => RecordUtils.getPrimaryKey(schema, r));
 
+        //return ConditionTreeFactory.matchIds(schema, ids);
     }
-    //static intersect(...trees: ConditionTree[]): ConditionTree {
-    //    const result = ConditionTreeFactory.group('And', trees);
-    //    const isEmptyAnd =
-    //      result instanceof ConditionTreeBranch &&
-    //      result.aggregator === 'And' &&
-    //      result.conditions.length === 0;
-    //
-    //    return isEmptyAnd ? null : result;
-    //  }
+
+    public static function matchIds(Collection $collection, array $ids): ConditionTree
+    {
+        $primaryKeyNames = SchemaUtils::getPrimaryKeys($collection);
+
+        if (count($primaryKeyNames) === 0) {
+            throw new ForestException('Collection must have at least one primary key');
+        }
+
+        foreach ($primaryKeyNames as $name) {
+            $operators = $collection->getFields()[$name]->getFilterOperators();
+
+            if (! in_array('Equal', $operators, true) || ! in_array('In', $operators, true)) {
+                throw new ForestException("Field '$name' must support operators: ['Equal', 'In']");
+            }
+        }
+
+        return self::matchFields($primaryKeyNames, $ids);
+    }
+
+    public static function intersect(array $trees): ?ConditionTree
+    {
+        $result = self::group('And', $trees);
+        $isEmptyAnd = $result instanceof ConditionTreeBranch &&
+            $result->getAggregator() === 'And' &&
+            count($result->getConditions()) === 0;
+
+        return $isEmptyAnd ? null : $result;
+    }
+
+    public static function union(array $trees): ConditionTree
+    {
+        return self::group('Or', $trees);
+    }
+
+    public static function fromArray(array $tree): ConditionTree
+    {
+        if (self::isLeaf($tree)) {
+            return new ConditionTreeLeaf($tree['field'], $tree['operator'], $tree['value']);
+        }
+
+        if (self::isBranch($tree)) {
+            $conditions = [];
+            foreach ($tree['conditions'] as $condition) {
+                $conditions[] = new ConditionTreeLeaf($condition['field'], $condition['operator'], $condition['value']);
+            }
+            $branch = new ConditionTreeBranch($tree['aggregator'], $conditions);
+
+            return count($branch->getConditions()) === 1 ? $branch->getConditions()[0] : $branch;
+        }
+
+        throw new ForestException('Failed to instantiate condition tree from array');
+    }
+
+    private static function group(string $aggregator, array $trees): ConditionTree
+    {
+        $conditions = collect($trees)
+            ->filter()
+            ->reduce(
+                static function ($currentConditions, $tree) {
+                    return $tree instanceof ConditionTreeBranch
+                        ? [...$currentConditions, ...$tree->getConditions()]
+                        : [...$currentConditions, $tree];
+                },
+                []
+            );
+
+        return count($conditions) === 1 ? $conditions[0] : new ConditionTreeBranch($aggregator, $conditions);
+    }
+
+    private static function matchFields(array $fields, array $values): ConditionTree
+    {
+        if (count($values) === 0) {
+            return new ConditionTreeBranch('Or', []);
+        }
+
+        if (count($values) === 1) {
+            $fieldValues = [];
+            // todo check with test
+            foreach ($values as $value) {
+                $fieldValues[] = $value[0];
+            }
+
+            return count($fieldValues) > 1
+                ? new ConditionTreeLeaf($fields[0], 'In', $fieldValues)
+                : new ConditionTreeLeaf($fields[0], 'Equal', $fieldValues[0]);
+        }
+
+//        const [firstField, ...otherFields] = fields;
+        $group = new IlluminateCollection();
+
+        foreach ($values as $value) {
+            if ($group->has($value[0])) {
+                $group->get($value[0])->push($value[1]);
+            } else {
+                $group->put($value[0], collect([$value[1]]));
+            }
+        }
+        // todo finish this with matchRecords()
+    }
+
+    private static function isLeaf(array $tree): bool
+    {
+        return array_key_exists('field', $tree)
+            && array_key_exists('operator', $tree)
+            && array_key_exists('value', $tree);
+    }
+
+    private static function isBranch(array $tree): bool
+    {
+        return array_key_exists('aggregator', $tree) && array_key_exists('conditions', $tree);
+    }
 }
---
+++ tests/DatasourceToolkit/Unit/Components/Query/ConditionTreeFactoryTest.php
@@ -0,0 +1,115 @@
+<?php
+
+use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\ConditionTreeFactory;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\ConditionTreeBranch;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\ConditionTreeLeaf;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Datasource;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Decorators\Schema\ColumnSchema;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Decorators\Schema\Concerns\PrimitiveType;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+
+test('intersect() should return the parameter when called with only one param', function () {
+    $tree = ConditionTreeFactory::intersect([new ConditionTreeLeaf('column', 'Equal', true)]);
+
+    expect($tree)->toEqual(new ConditionTreeLeaf('column', 'Equal', true));
+});
+
+test('intersect() should ignore null params', function () {
+    $tree = ConditionTreeFactory::intersect([null, new ConditionTreeLeaf('column', 'Equal', true), null]);
+
+    expect($tree)->toEqual(new ConditionTreeLeaf('column', 'Equal', true));
+});
+
+test('intersect() multiple trees should return the tree', function () {
+    $conditionTree = new ConditionTreeLeaf('column', 'Equal', true);
+    $otherConditionTree = new ConditionTreeLeaf('otherColumn', 'Equal', true);
+    $tree = ConditionTreeFactory::intersect([$conditionTree, $otherConditionTree]);
+
+    expect($tree)->toEqual(new ConditionTreeBranch('And', [$conditionTree, $otherConditionTree]));
+});
+
+test('intersect() should merge And trees', function () {
+    $conditionTree = new ConditionTreeLeaf('column', 'Equal', true);
+    $otherConditionTree = new ConditionTreeLeaf('otherColumn', 'Equal', true);
+    $tree = ConditionTreeFactory::intersect(
+        [
+            new ConditionTreeBranch('And', [$conditionTree]),
+            new ConditionTreeBranch('And', [$otherConditionTree]),
+        ]
+    );
+
+    expect($tree)->toEqual(new ConditionTreeBranch('And', [$conditionTree, $otherConditionTree]));
+});
+
+test('matchIds() with a collection with no pk should raise error', function () {
+    $collection = new Collection(new Datasource(), 'cars');
+    $collection->addFields(['column' => new ColumnSchema(columnType: PrimitiveType::STRING)]);
+
+    expect(static fn () => ConditionTreeFactory::matchIds($collection, [[]]))
+        ->toThrow(ForestException::class, 'Collection must have at least one primary key');
+});
+
+test('fromArray() should crash when calling with badly formatted array', function () {
+    expect(static fn () => ConditionTreeFactory::fromArray([]))
+        ->toThrow(ForestException::class, '🌳🌳🌳 Failed to instantiate condition tree from array');
+});
+
+test('fromArray() should work with a simple case', function () {
+    $tree = ConditionTreeFactory::fromArray(
+        [
+            'field'    => 'field',
+            'operator' => 'Equal',
+            'value'    => 'something',
+        ]
+    );
+
+    expect($tree)->toEqual(new ConditionTreeLeaf('field', 'Equal', 'something'));
+});
+
+test('fromArray() should remove useless aggregators from the frontend', function () {
+    $tree = ConditionTreeFactory::fromArray(
+        [
+            'aggregator' => 'And',
+            'conditions' => [
+                [
+                    'field'    => 'field',
+                    'operator' => 'Equal',
+                    'value'    => 'something',
+                ],
+            ],
+        ]
+    );
+
+    expect($tree)->toEqual(new ConditionTreeLeaf('field', 'Equal', 'something'));
+});
+
+test('fromArray() should work with an aggregator', function () {
+    $tree = ConditionTreeFactory::fromArray(
+        [
+            'aggregator' => 'And',
+            'conditions' => [
+                [
+                    'field'    => 'field',
+                    'operator' => 'Equal',
+                    'value'    => 'something',
+                ],
+                [
+                    'field'    => 'field',
+                    'operator' => 'Equal',
+                    'value'    => 'something',
+                ],
+            ],
+        ]
+    );
+
+    expect($tree)->toEqual(
+        new ConditionTreeBranch(
+            'And',
+            [
+                new ConditionTreeLeaf('field', 'Equal', 'something'),
+                new ConditionTreeLeaf('field', 'Equal', 'something'),
+            ]
+        ),
+    );
+});
