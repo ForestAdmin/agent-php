3d61b7f9c573ac02597714c88d855ade63edb54c
Matt <matthv@gmail.com>
2022-08-01 15:57:27 +0200

    chore: add Percentage chart

--- src/DatasourceToolkit/Components/Charts/PercentageChart.php
+++ src/DatasourceToolkit/Components/Charts/PercentageChart.php
@@ -4,8 +4,12 @@ namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Charts;
 
 class PercentageChart extends Chart
 {
+    public function __construct(protected int $value)
+    {
+    }
+
     public function serialize(): array
     {
-        return $this->data[0];
+        return $this->value;
     }
 }
