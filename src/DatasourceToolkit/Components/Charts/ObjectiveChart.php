9e7a48a9efc3026ebaf12302b59215b9c68be30f
Matt <matthv@gmail.com>
2022-08-16 16:15:17 +0200

    fix(chart): remove useless validator on Objective chart

--- src/DatasourceToolkit/Components/Charts/ObjectiveChart.php
+++ src/DatasourceToolkit/Components/Charts/ObjectiveChart.php
@@ -12,8 +12,6 @@ class ObjectiveChart extends Chart
 
     public function serialize()
     {
-        ChartValidator::validate((! array_key_exists('objective', $this->data) || ! array_key_exists('value', $this->data)), $this->data, "'objective', 'value'");
-
         return [
             'value'     => $this->value,
             'objective' => $this->objective,
