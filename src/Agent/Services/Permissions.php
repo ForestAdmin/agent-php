1d545addb019e1f4b42504ca5037360a81779118
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-08-17 17:20:21 +0200

    feat: add "can" function

--- src/Agent/Services/Permissions.php
+++ src/Agent/Services/Permissions.php
@@ -9,6 +9,9 @@ use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Caller;
 
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\ConditionTreeFactory;
 
+use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+use Illuminate\Support\Collection as IlluminateCollection;
+use Symfony\Component\HttpFoundation\Response;
 use function ForestAdmin\cache;
 
 use Illuminate\Support\Str;
@@ -24,8 +27,7 @@ class Permissions
 
     public function invalidateCache(int $renderingId): void
     {
-        // todo
-        //app('cache')->delete(....)
+        Cache::forget("permissions.$renderingId");
     }
 
     public function canChart(): void
@@ -33,9 +35,23 @@ class Permissions
         // todo Checks that a charting query is in the list of allowed queries
     }
 
-    public function can(): void
+    public function can(string $action, string $collectionName, $allowFetch = true): bool
     {
-        // todo Check if a user is allowed to perform a specific action
+        $this->invalidateCache($this->caller->getRenderingId());
+        $permissions = $this->getRenderingPermissions($this->caller->getRenderingId());
+        $isAllowed = $permissions->get('actions')->get($action)->contains($this->caller->getId());
+
+        if (! $isAllowed && $allowFetch) {
+            $this->invalidateCache($this->caller->getRenderingId());
+
+            return $this->can($action, $collectionName, false);
+        }
+
+        if (! $isAllowed) {
+            throw new ForestException('Forbidden', Response::HTTP_FORBIDDEN);
+        }
+
+        return $isAllowed;
     }
 
     public function getScope(): void
@@ -43,7 +59,7 @@ class Permissions
         // todo
     }
 
-    private function getRenderingPermissions(int $renderingId): array
+    private function getRenderingPermissions(int $renderingId): IlluminateCollection
     {
         return Cache::remember(
             "permissions.$renderingId",
@@ -51,37 +67,39 @@ class Permissions
                 $permissions = ForestHttpApi::getPermissions($renderingId);
 
                 // todo decode Chart
-                return [
-                    'actions' => $this->decodeActionPermissions($permissions),
-                    'scopes'  => $this->decodeScopePermissions($permissions, $renderingId),
-                ];
+                return collect(
+                    [
+                        'actions' => $this->decodeActionPermissions($permissions),
+                        'scopes'  => $this->decodeScopePermissions($permissions, $renderingId),
+                    ]
+                );
             },
             self::TTL
         );
     }
 
-    private function decodeActionPermissions(array $rawPermissions): array
+    private function decodeActionPermissions(array $rawPermissions): IlluminateCollection
     {
-        $actions = [];
+        $actions = collect();
         foreach ($rawPermissions['data']['collections'] as $collectionName => $permission) {
             foreach ($permission['collection'] as $actionName => $userIds) {
                 $shortName = Str::before($actionName, 'Enabled');
                 $userIds = $userIds instanceof Boolean ? [$this->caller->getId()] : $userIds;
-                $actions["$shortName:$collectionName"] = $userIds;
+                $actions->put("$shortName:$collectionName", collect($userIds));
             }
 
             foreach ($permission['actions'] as $actionName => $actionPermissions) {
                 $userIds = $actionPermissions['triggerEnabled'];
                 $userIds = $userIds instanceof Boolean ? [$this->caller->getId()] : $userIds;
 
-                $actions["custom:$actionName:$collectionName"] = $userIds;
+                $actions->put("custom:$actionName:$collectionName", collect($userIds));
             }
         }
 
         return $actions;
     }
 
-    private function decodeScopePermissions(array $rawPermissions, int $renderingId): array
+    private function decodeScopePermissions(array $rawPermissions, int $renderingId): IlluminateCollection
     {
         $scopes = [];
         if (isset($rawPermissions['data']['renderings'][$renderingId])) {
@@ -95,6 +113,6 @@ class Permissions
             }
         }
 
-        return $scopes;
+        return collect($scopes);
     }
 }
