17f58ecaef03667a279902806edb340a1963ab6f
Matt <matthv@gmail.com>
2022-09-28 16:02:07 +0200

    chore: add listing test

--- src/DatasourceToolkit/Utils/Record.php
+++ src/DatasourceToolkit/Utils/Record.php
@@ -14,22 +14,4 @@ class Record
             fn ($pk) => $record[$pk] ?? throw new ForestException("Missing primary key: $pk")
         )->toArray();
     }
-<<<<<<< HEAD
-=======
-
-    public static function getToManyRelation(Collection $collection, string $relationName): RelationSchema
-    {
-        $relation = $collection->getFields()[$relationName];
-
-        if (! $relation) {
-            throw new ForestException("Relation $relationName not found");
-        }
-
-        if ($relation->getType() !== 'OneToMany' && $relation->getType() !== 'ManyToMany') {
-            throw new ForestException("Relation $relationName has invalid type should be one of OneToMany or ManyToMany.");
-        }
-
-        return $relation;
-    }
->>>>>>> 96c9102 (feat: update ConditionTreeFactory )
 }
