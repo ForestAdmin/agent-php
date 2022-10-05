5d64a7b39f6bb179b111059ca77fb9ce2772a756
Matt <matthv@gmail.com>
2022-10-05 16:38:10 +0200

    refactor: merge BodyParser utils to Id utils

--- src/Agent/Utils/Id.php
+++ src/Agent/Utils/Id.php
@@ -2,6 +2,7 @@
 
 namespace ForestAdmin\AgentPHP\Agent\Utils;
 
+use ForestAdmin\AgentPHP\Agent\Http\Request;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Utils\Schema as SchemaUtils;
@@ -53,5 +54,19 @@ class Id
         $values = collect($packedIds)->map(fn ($item) => self::unpackId($collection, $item));
 
         return $values->all();
+    }
+
+    public static function parseSelectionIds(Collection $collection, Request $request): array
+    {
+        $attributes = $request->input('data.attributes');
+
+        $areExcluded = $attributes && array_key_exists('all_records', $attributes) ? $attributes['all_records'] : false;
+        $inputIds = $attributes && array_key_exists('ids', $attributes)
+            ? $attributes['ids']
+            : collect($request->get('data'))->map(fn ($item) => $item['id'])->all();
+
+        $ids = self::unpackIds($collection, $areExcluded ? $attributes['all_records_ids_excluded'] : $inputIds);
+
+        return compact('areExcluded', 'ids');
     }
 }
