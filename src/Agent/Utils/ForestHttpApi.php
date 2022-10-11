1c39b544c5715832bbb5076bd9c745994354422f
Matt <matthv@gmail.com>
2022-10-11 11:49:23 +0200

    chore: add check endpoint forest/apimaps/hashcheck on ForestAdminHttpDriver

--- src/Agent/Utils/ForestHttpApi.php
+++ src/Agent/Utils/ForestHttpApi.php
@@ -4,23 +4,25 @@ namespace ForestAdmin\AgentPHP\Agent\Utils;
 
 use ForestAdmin\AgentPHP\Agent\Http\ForestApiRequester;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+use Illuminate\Support\Str;
 
 class ForestHttpApi
 {
-    public static function hasSchema(array $httpOptions, string $hash): bool
+    public static function hasSchema(string $schemaFileHash): bool
     {
-//        try {
-//            const response = await superagent
-//            .post(new URL('/forest/apimaps/hashcheck', options.forestServerUrl).toString())
-//            .send({ schemaFileHash: hash })
-//            .set('forest-secret-key', options.envSecret);
-//
-//          return !response?.body?.sendSchema;
-//        } catch (e) {
-//            this.handleResponseError(e);
-//        }
+        try {
+            $forestApi = new ForestApiRequester();
+            $response = $forestApi->post(
+                '/forest/apimaps/hashcheck',
+                [],
+                compact('schemaFileHash'),
+            );
+            $body = json_decode($response->getBody(), true, 512, JSON_THROW_ON_ERROR);
 
-        return true;
+            return $body['sendSchema'];
+        } catch (\Exception $e) {
+            self::handleResponseError($e);
+        }
     }
 
     public static function uploadSchema(array $jsonApiDocument): void
@@ -33,7 +35,7 @@ class ForestHttpApi
                 $jsonApiDocument
             );
         } catch (\Exception $e) {
-            dd($e);
+            self::handleResponseError($e);
         }
     }
 
@@ -53,7 +55,28 @@ class ForestHttpApi
 
             return $body;
         } catch (\Exception $e) {
-            // todo this.handleResponseError(e);
+            self::handleResponseError($e);
         }
+    }
+
+    public static function handleResponseError(\Exception $e)
+    {
+        if (Str::contains($e->getMessage(), 'certificate')) {
+            throw new ForestException('ForestAdmin server TLS certificate cannot be verified. Please check that your system time is set properly.');
+        }
+
+        if ($e->getCode() === 0 || $e->getCode() === 502) {
+            throw new ForestException('Failed to reach ForestAdmin server. Are you online?');
+        }
+
+        if ($e->getCode() === 404) {
+            throw new ForestException('ForestAdmin server failed to find the project related to the envSecret you configured. Can you check that you copied it properly in the Forest initialization?');
+        }
+
+        if ($e->getCode() === 503) {
+            throw new ForestException('Forest is in maintenance for a few minutes. We are upgrading your experience in the forest. We just need a few more minutes to get it right.');
+        }
+
+        throw new ForestException('An unexpected error occured while contacting the ForestAdmin server. Please contact support@forestadmin.com for further investigations.');
     }
 }
