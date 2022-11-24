83ae7a30ff0da94464b1dbae7649d5bcdb2b5f65
Matt <matthv@gmail.com>
2022-11-24 17:31:44 +0100

    feat(primitivetype): add list

--- src/DatasourceToolkit/Schema/Concerns/PrimitiveType.php
+++ src/DatasourceToolkit/Schema/Concerns/PrimitiveType.php
@@ -14,4 +14,11 @@ final class PrimitiveType
     public const STRING = 'String';
     public const TIMEONLY = 'Timeonly';
     public const UUID = 'Uuid';
+
+    public static function tree(): array
+    {
+        return array_values(
+            (new \ReflectionClass(self::class))->getConstants()
+        );
+    }
 }
