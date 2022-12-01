7c90ab6fc06c1f689f142cd27443969d22a289b6
Matt <matthv@gmail.com>
2022-12-01 12:01:28 +0100

    fix: add colletionContract over collection into record & conditionTree

--- src/DatasourceToolkit/Utils/Record.php
+++ src/DatasourceToolkit/Utils/Record.php
@@ -2,14 +2,14 @@
 
 namespace ForestAdmin\AgentPHP\DatasourceToolkit\Utils;
 
-use ForestAdmin\AgentPHP\DatasourceToolkit\Collection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Contracts\CollectionContract;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class Record
 {
-    public static function getPrimaryKeys(Collection $schema, array $record): array
+    public static function getPrimaryKeys(CollectionContract $schema, array $record): array
     {
         return collect(Schema::getPrimaryKeys($schema))->map(
             fn ($pk) => $record[$pk] ?? throw new ForestException("Missing primary key: $pk")
