46debfdf5625dd7fb30df88a80fe012bf10bec50
Matt <matthv@gmail.com>
2022-12-13 14:33:18 +0100

    chore: add remaining test on Aggregation

--- tests/DatasourceToolkit/Unit/Components/Query/AggregationTest.php
+++ tests/DatasourceToolkit/Unit/Components/Query/AggregationTest.php
@@ -2,85 +2,280 @@
 
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\Aggregation;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\Projection\Projection;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+
+test('validate() should throw if the operation value is not allowed', function () {
+    expect(fn () => new Aggregation('Foo'))->toThrow(ForestException::class, '🌳🌳🌳 Aggregate operation Foo not allowed');
+});
+
+test('validate() should not throw if the operation is allowed', function () {
+    new Aggregation('Count');
+})->expectNotToPerformAssertions();
 
 test('getProjection() should work with a null field and an empty groups', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
     expect($aggregation->getProjection())->toEqual(new Projection([]));
 });
 
 test('getProjection() should work with a field and a groups', function () {
-    $aggregation = new Aggregation('count', 'aggregateField', [['field' => 'groupField']]);
+    $aggregation = new Aggregation('Count', 'aggregateField', [['field' => 'groupField']]);
 
     expect($aggregation->getProjection())->toEqual(new Projection(['aggregateField', 'groupField']));
 });
 
 test('getOperation() should work ', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
-    expect($aggregation->getOperation())->toEqual('count');
+    expect($aggregation->getOperation())->toEqual('Count');
 });
 
 test('getField() should work with a field', function () {
-    $aggregation = new Aggregation('count', 'aggregateField');
+    $aggregation = new Aggregation('Count', 'aggregateField');
 
     expect($aggregation->getField())->toEqual('aggregateField');
 });
 
 test('getField() should return null with no field', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
     expect($aggregation->getField())->toBeNull();
 });
 
 test('getGroups() should work with no grouping', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
     expect($aggregation->getGroups())->toEqual([]);
 });
 
 test('getGroups() should work with groups', function () {
-    $aggregation = new Aggregation('count', null, [['field' => 'groupField']]);
+    $aggregation = new Aggregation('Count', null, [['field' => 'groupField']]);
 
     expect($aggregation->getGroups())->toEqual([['field' => 'groupField']]);
 });
 
 test('override() should work with one arg', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
-    expect($aggregation->override(operation: 'sum'))->toEqual(new Aggregation('sum'));
+    expect($aggregation->override(operation: 'Sum'))->toEqual(new Aggregation('Sum'));
 });
 
 test('override() should work with all args', function () {
-    $aggregation = new Aggregation('count');
+    $aggregation = new Aggregation('Count');
 
-    expect($aggregation->override(operation: 'sum', field: 'aggregateField', groups: [['field' => 'groupField']]))
-        ->toEqual(new Aggregation('sum', 'aggregateField', [['field' => 'groupField']]));
+    expect($aggregation->override(operation: 'Sum', field: 'aggregateField', groups: [['field' => 'groupField']]))
+        ->toEqual(new Aggregation('Sum', 'aggregateField', [['field' => 'groupField']]));
+});
+
+test('apply() should work with records, timezone and limit null', function () {
+    $aggregation = new Aggregation('Count');
+    $records = [
+        ['id' => 1],
+        ['id' => 1],
+        ['id' => 1],
+        ['id' => 2],
+        ['id' => 2],
+        ['id' => 2],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [],
+            'value' => 3,
+        ],
+        [
+            'group' => [],
+            'value' => 3,
+        ],
+    ]);
+});
+
+test('apply() should work with records, timezone and limit not null', function () {
+    $aggregation = new Aggregation('Count');
+    $records = [
+        ['id' => 1],
+        ['id' => 1],
+        ['id' => 1],
+        ['id' => 1],
+        ['id' => 2],
+        ['id' => 2],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris', 1))->toEqual([
+        [
+            'group' => [],
+            'value' => 4,
+        ],
+    ]);
+});
+
+test('apply() should work with records, timezone and limit null on Avg operation', function () {
+    $aggregation = new Aggregation('Avg', 'id');
+    $records = [
+        ['id' => 1],
+        ['id' => 2],
+        ['id' => 2],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [],
+            'value' => 1,
+        ],
+        [
+            'group' => [],
+            'value' => 2,
+        ],
+    ]);
+});
+
+test('apply() should work with group field on year', function () {
+    $aggregation = new Aggregation('Avg', 'field', [['field' => 'groupField', 'operation' => 'Year']]);
+    $records = [
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 10, 'groupField' => '2022-01-02'],
+        ['field' => 1, 'groupField' => '2023-07-11'],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [
+                'groupField' => '2022-01-01',
+            ],
+            'value' => 5,
+        ],
+        [
+            'group' => [
+                'groupField' => '2022-01-01',
+            ],
+            'value' => 10,
+        ],
+        [
+            'group' => [
+                'groupField' => '2023-01-01',
+            ],
+            'value' => 1,
+        ],
+    ]);
+});
+
+test('apply() should work with group field on month', function () {
+    $aggregation = new Aggregation('Avg', 'field', [['field' => 'groupField', 'operation' => 'Month']]);
+    $records = [
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 10, 'groupField' => '2022-01-02'],
+        ['field' => 1, 'groupField' => '2023-07-11'],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [
+                'groupField' => '2022-05-01',
+            ],
+            'value' => 5,
+        ],
+        [
+            'group' => [
+                'groupField' => '2022-01-01',
+            ],
+            'value' => 10,
+        ],
+        [
+            'group' => [
+                'groupField' => '2023-07-01',
+            ],
+            'value' => 1,
+        ],
+    ]);
+});
+
+test('apply() should work with group field on day', function () {
+    $aggregation = new Aggregation('Avg', 'field', [['field' => 'groupField', 'operation' => 'Day']]);
+    $records = [
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 10, 'groupField' => '2022-01-02'],
+        ['field' => 1, 'groupField' => '2023-07-11'],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [
+                'groupField' => '2022-05-01',
+            ],
+            'value' => 5,
+        ],
+        [
+            'group' => [
+                'groupField' => '2022-01-02',
+            ],
+            'value' => 10,
+        ],
+        [
+            'group' => [
+                'groupField' => '2023-07-11',
+            ],
+            'value' => 1,
+        ],
+    ]);
+});
+
+test('apply() should work with group field on week', function () {
+    $aggregation = new Aggregation('Avg', 'field', [['field' => 'groupField', 'operation' => 'Week']]);
+    $records = [
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 5, 'groupField' => '2022-05-01'],
+        ['field' => 10, 'groupField' => '2022-01-02'],
+        ['field' => 1, 'groupField' => '2023-07-11'],
+    ];
+
+    expect($aggregation->apply($records, 'Europe/Paris'))->toEqual([
+        [
+            'group' => [
+                'groupField' => '2022-05-01',
+            ],
+            'value' => 5,
+        ],
+        [
+            'group' => [
+                'groupField' => '2022-01-01',
+            ],
+            'value' => 10,
+        ],
+        [
+            'group' => [
+                'groupField' => '2023-07-01',
+            ],
+            'value' => 1,
+        ],
+    ]);
 });
 
 test('nest() should work ', function () {
-    $aggregation = new Aggregation('sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]);
+    $aggregation = new Aggregation('Sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]);
 
     expect($aggregation->nest('prefix'))
-        ->toEqual(new Aggregation('sum', 'prefix:aggregateField', [['field' => 'prefix:groupField', 'operation' => 'Week']]));
+        ->toEqual(new Aggregation('Sum', 'prefix:aggregateField', [['field' => 'prefix:groupField', 'operation' => 'Week']]));
 });
 
 test('nest() should work with null prefix', function () {
-    $aggregation = new Aggregation('sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]);
+    $aggregation = new Aggregation('Sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]);
 
     expect($aggregation->nest(null))
-        ->toEqual(new Aggregation('sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]));
+        ->toEqual(new Aggregation('Sum', 'aggregateField', [['field' => 'groupField', 'operation' => 'Week']]));
 });
 
 test('toArray() should work', function () {
-    $aggregation = new Aggregation('sum', 'aggregateField', [['field' => 'groupField']]);
+    $aggregation = new Aggregation('Sum', 'aggregateField', [['field' => 'groupField']]);
 
     expect($aggregation->toArray())
         ->toEqual(
             [
-                'operation'  => 'sum',
-                'field'      => 'aggregateField',
-                'groups'     => [['field' => 'groupField']],
+                'operation' => 'Sum',
+                'field'     => 'aggregateField',
+                'groups'    => [['field' => 'groupField']],
             ]
         );
 });
