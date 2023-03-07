28cf811785860213589c1dab2fa26184fa8b51b1
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-03-07 12:01:42 +0100

    feat(action): add methods for actionContext

--- src/DatasourceCustomizer/Decorators/Actions/Context/ActionContextSingle.php
+++ src/DatasourceCustomizer/Decorators/Actions/Context/ActionContextSingle.php
@@ -4,4 +4,24 @@ namespace ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\Actions\Context;
 
 class ActionContextSingle extends ActionContext
 {
+    public function getRecord(array $fields): array
+    {
+        $records = $this->getRecords($fields);
+
+        return $records[0] ?? [];
+    }
+
+    public function getRecordId(): array
+    {
+        $compositeId = $this->getCompositeRecordId();
+
+        return $compositeId[0] ?? [];
+    }
+
+    public function getCompositeRecordId(): array
+    {
+        $ids = $this->getCompositeRecordIds();
+
+        return $ids[0] ?? [];
+    }
 }
