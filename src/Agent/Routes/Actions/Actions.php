a7f553c289e8d4aed7461ac400fac16572633986
Matt <matthv@gmail.com>
2023-03-09 17:39:01 +0100

    chore: remove old comments on action route

--- src/Agent/Routes/Actions/Actions.php
+++ src/Agent/Routes/Actions/Actions.php
@@ -68,18 +68,8 @@ class Actions extends AbstractAuthenticatedRoute
         $this->build($args);
         $scope = $this->permissions->getScope($this->collection);
         $filter = ContextFilterFactory::buildPaginated($this->collection, $this->request, $scope);
-        //(Caller $caller, string $name, array $data, ?Filter $filter = null)
 
         return $this->collection->execute($this->caller, $this->actionName, $this->request->get('data'), $filter);
-
-//        $this->build($args);
-//        $this->permissions->canChart($this->request);
-//        $scope = $this->permissions->getScope($this->collection);
-//        $this->filter = ContextFilterFactory::build($this->collection, $this->request, $scope);
-//        $this->setType($this->request->get('type'));
-//        $this->setCaller(QueryStringParser::parseCaller($this->request));
-//
-//        return ['content' => JsonApi::renderChart($this->{'make' . $this->type}())];
     }
 
     public function handleHookRequest(array $args = []): array
@@ -92,32 +82,6 @@ class Actions extends AbstractAuthenticatedRoute
         $fields = $this->collection->getForm($this->caller, $this->actionName, $data, $filter);
 
         return ['content' => ['fields' => collect($fields)->map(fn ($field) => GeneratorAction::buildFieldSchema(AgentFactory::get('datasource'), $field))]];
-
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//        const body = context.request.body as any;
-//        const { id: userId } = context.state.user;
-//
-//        await this.actionAuthorizationService.assertCanRequestCustomActionParameters(
-//            userId,
-//            this.actionName,
-//            this.collection.name,
-//        );
-//
-//        const { dataSource } = this.collection;
-//        const forestFields = body?.data?.attributes?.fields;
-//        const data = forestFields
-//            ? ForestValueConverter.makeFormDataFromFields(dataSource, forestFields)
-//            : null;
-//
-//        const caller = QueryStringParser.parseCaller(context);
-//        const filter = await this.getRecordSelection(context);
-//        const fields = await this.collection.getForm(caller, this.actionName, data, filter);
-//
-//        context.response.body = {
-//            fields: fields.map(field =>
-//            SchemaGeneratorActions.buildFieldSchema(this.collection.dataSource, field),
-//          ),
-//        };
     }
 
     private function getRecordSelection(bool $includeUserScope = true): Filter
@@ -145,39 +109,4 @@ class Actions extends AbstractAuthenticatedRoute
 
         return $filter;
     }
-
-//        private async getRecordSelection(context: Context, includeUserScope = true): Promise<Filter> {
-//            // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//            const body = context.request.body as any;
-//            const attributes = body?.data?.attributes;
-//
-//            // Match user filter + search + scope? + segment.
-//            const scope = includeUserScope
-//                ? await this.services.authorization.getScope(this.collection, context)
-//              : null;
-//            let filter = ContextFilterFactory.build(this.collection, context, scope);
-//
-//            // Restrict the filter to the selected records for single or bulk actions.
-//            if (this.collection.schema.actions[this.actionName].scope !== 'Global') {
-//              const selectionIds = BodyParser.parseSelectionIds(this.collection.schema, context);
-//              let selectedIds = ConditionTreeFactory.matchIds(this.collection.schema, selectionIds.ids);
-//              if (selectionIds.areExcluded) selectedIds = selectedIds.inverse();
-//
-//              filter = filter.override({
-//                conditionTree: ConditionTreeFactory.intersect(filter.conditionTree, selectedIds),
-//              });
-    //        }
-    //
-    //        // Restrict the filter further for the "related data" page.
-    //        if (attributes?.parent_association_name) {
-    //            const caller = QueryStringParser.parseCaller(context);
-    //            const relation = attributes?.parent_association_name;
-    //              const parent = this.dataSource.getCollection(attributes.parent_collection_name);
-    //              const parentId = IdUtils.unpackId(parent.schema, attributes.parent_collection_id);
-    //
-    //              filter = await FilterFactory.makeForeignFilter(parent, parentId, relation, caller, filter);
-    //            }
-    //
-    //            return filter;
-    //          }
 }
