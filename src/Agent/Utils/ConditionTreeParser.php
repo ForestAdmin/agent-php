d6d23563a21c8858e3e0c86e90d9371e496d8924
Matt <matthv@gmail.com>
2023-03-13 16:02:16 +0100

    fix: conditionTreeParser on parseValue

--- src/Agent/Utils/ConditionTreeParser.php
+++ src/Agent/Utils/ConditionTreeParser.php
@@ -43,7 +43,7 @@ class ConditionTreeParser
     {
         $schema = CollectionUtils::getFieldSchema($collection, $leaf['field']);
 
-        if ($leaf['operator'] === 'In' && is_string($leaf['value'])) {
+        if (ucfirst($leaf['operator']) === 'In' && is_string($leaf['value'])) {
             $values = collect(explode(',', $leaf['value']))
                 ->map(fn ($item) => trim($item));
 
@@ -54,7 +54,7 @@ class ConditionTreeParser
             }
 
             if ($schema->getColumnType() === PrimitiveType::NUMBER) {
-                return $values->each(fn ($item) => (float) $item)
+                return $values->map(fn ($item) => (float) $item)
                     ->filter(fn ($number) => ! is_nan($number) && is_finite($number))
                     ->all();
             }
