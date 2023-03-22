f765a018b71824a6e5f0c61b0487f5a3a53e4365
Matt <matthv@gmail.com>
2023-03-22 11:20:59 +0100

    chore: extends WriteCustomizationContext to CollectionCustomizationContext

--- src/DatasourceCustomizer/Decorators/Write/WriteReplace/WriteCustomizationContext.php
+++ src/DatasourceCustomizer/Decorators/Write/WriteReplace/WriteCustomizationContext.php
@@ -2,14 +2,15 @@
 
 namespace ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\Write\WriteReplace;
 
+use ForestAdmin\AgentPHP\DatasourceCustomizer\Context\CollectionCustomizationContext;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Caller;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Contracts\CollectionContract;
 
-class WriteCustomizationContext //todo extends CollectionCustomizationContext
+class WriteCustomizationContext extends CollectionCustomizationContext
 {
     public function __construct(CollectionContract $collection, Caller $caller, protected string $action, protected array $record)
     {
-        //parent::__construct($collection, $caller);
+        parent::__construct($collection, $caller);
     }
 
     public function getAction(): string
