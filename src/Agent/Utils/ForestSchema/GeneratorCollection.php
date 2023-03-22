ce3367e21994e0838f294a57f8f38ffab9b96f95
NicolasAlexandre <nicolasalexandre9@gmail.com>
2023-03-22 16:12:59 +0100

    fix(schema): sort actions by id and fields by name in the schema (#37)

--- src/Agent/Utils/ForestSchema/GeneratorCollection.php
+++ src/Agent/Utils/ForestSchema/GeneratorCollection.php
@@ -9,8 +9,8 @@ class GeneratorCollection
     public static function buildSchema(CollectionContract $collection): array
     {
         return [
-            'actions'              => $collection->getActions()->map(fn ($action, $name) => GeneratorAction::buildSchema($collection, $name))->values()->toArray(),
-            'fields'               => $collection->getFields()->map(fn ($field, $name) => GeneratorField::buildSchema($collection, $name))->values()->toArray(),
+            'actions'              => $collection->getActions()->map(fn ($action, $name) => GeneratorAction::buildSchema($collection, $name))->sortBy('id')->values()->toArray(),
+            'fields'               => $collection->getFields()->map(fn ($field, $name) => GeneratorField::buildSchema($collection, $name))->sortBy('field')->values()->toArray(),
             'icon'                 => null,
             'integration'          => null,
             'isReadOnly'           => $collection->getFields()->every(fn ($field) => $field->getType() === 'Column' && $field->isReadOnly()),
