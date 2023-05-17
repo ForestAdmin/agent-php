fef0a548a0a7ee59f157febf9970ce2b5827b9f4
Matt <matthv@gmail.com>
2023-05-17 14:48:28 +0200

    chore: add new http exception & update other exception with new name attribute

--- src/Agent/Http/Exceptions/ConflictError.php
+++ src/Agent/Http/Exceptions/ConflictError.php
@@ -5,11 +5,11 @@ namespace ForestAdmin\AgentPHP\Agent\Http\Exceptions;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class ForbiddenError extends HttpException
+class ConflictError extends HttpException
 {
-    public function __construct(string $message, array $headers = [], protected string $name = 'ForbiddenError')
+    public function __construct(string $message, array $headers = [], protected string $name = 'ConflictError')
     {
-        parent::__construct(Response::HTTP_FORBIDDEN, $message, null, $headers);
+        parent::__construct(Response::HTTP_CONFLICT, $message, null, $headers);
     }
 
     public function getName(): string
--- src/Agent/Http/Exceptions/RequireApproval.php
+++ src/Agent/Http/Exceptions/RequireApproval.php
@@ -5,9 +5,9 @@ namespace ForestAdmin\AgentPHP\Agent\Http\Exceptions;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
-class ForbiddenError extends HttpException
+class RequireApproval extends HttpException
 {
-    public function __construct(string $message, array $headers = [], protected string $name = 'ForbiddenError')
+    public function __construct(string $message, array $headers = [], protected string $name = 'RequireApproval', protected array $data = [])
     {
         parent::__construct(Response::HTTP_FORBIDDEN, $message, null, $headers);
     }
@@ -15,5 +15,10 @@ class ForbiddenError extends HttpException
     public function getName(): string
     {
         return $this->name;
+    }
+
+    public function getData(): array
+    {
+        return $this->data;
     }
 }
