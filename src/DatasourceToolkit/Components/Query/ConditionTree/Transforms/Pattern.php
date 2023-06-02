d4d0026fa5a516ee4a3f3b42e76eded0303530ad
Matt <matthv@gmail.com>
2023-06-02 16:07:20 +0200

    chore: pattern add match logic

--- src/DatasourceToolkit/Components/Query/ConditionTree/Transforms/Pattern.php
+++ src/DatasourceToolkit/Components/Query/ConditionTree/Transforms/Pattern.php
@@ -3,6 +3,7 @@
 namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Transforms;
 
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Operators;
+use Illuminate\Support\Str;
 
 final class Pattern
 {
@@ -17,6 +18,34 @@ final class Pattern
         ];
     }
 
+    private function like(?string $value, string $pattern, bool $caseSensitive): bool
+    {
+        if (! $value) {
+            return false;
+        }
+
+        $regexp = Str::of($pattern)->replaceMatches('/([\.\\\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:\-])/', '\\$1')
+            ->replaceMatches('/%/', '.*')
+            ->replaceMatches('/_/', '.');
+
+        return preg_match_all('#^'.$regexp.'\z#' . ($caseSensitive ? '' : 'i'), $value) === 1;
+    }
+
+    public static function match(bool $caseSensitive): array
+    {
+        return [
+            'dependsOn' => [Operators::MATCH],
+            'forTypes'  => ['String'],
+            'replacer'  => function ($leaf) use ($caseSensitive) {
+                $regex = Str::of($leaf->getValue())->replaceMatches('/([\.\\\+\*\?\[\^\]\$\(\)\{\}\=\!\<\>\|\:\-])/', '\\$1')
+                    ->replaceMatches('/%/', '.*')
+                    ->replaceMatches('/_/', '.');
+
+                return $leaf->override(operator: Operators::MATCH, value: '/^'.$regex.'$/' . ($caseSensitive ? '' : 'i'));
+            },
+        ];
+    }
+
     public static function patternTransforms(): array
     {
         return [
@@ -26,6 +55,8 @@ final class Pattern
             Operators::ICONTAINS    => [self::likes(fn ($value) => '%' . $value . '%', false)],
             Operators::ISTARTS_WITH => [self::likes(fn ($value) => $value . '%', false)],
             Operators::IENDS_WITH   => [self::likes(fn ($value) => '%' . $value, false)],
+            Operators::ILIKE        => [self::match(false)],
+            Operators::LIKE         => [self::match(true)],
         ];
     }
 }
