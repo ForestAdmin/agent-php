443db6fe511209d981990091d0dab13a595069f7
Matt <matthv@gmail.com>
2023-06-07 14:37:43 +0200

    chore: add test on Pattern

--- tests/DatasourceToolkit/Unit/Components/Query/ConditionTree/Transforms/PatternTest.php
+++ tests/DatasourceToolkit/Unit/Components/Query/ConditionTree/Transforms/PatternTest.php
@@ -4,7 +4,6 @@ use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Nodes\
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Operators;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Transforms\Pattern;
 
-
 test('Contains should be rewritten', function () {
     $comparisons = Pattern::patternTransforms();
     expect($comparisons[Operators::CONTAINS][0]['replacer'](new ConditionTreeLeaf('column', Operators::CONTAINS, 'something'), 'Europe/Paris'))
@@ -39,4 +38,18 @@ test('IEndsWith should be rewritten', function () {
     $comparisons = Pattern::patternTransforms();
     expect($comparisons[Operators::IENDS_WITH][0]['replacer'](new ConditionTreeLeaf('column', Operators::ENDS_WITH, 'something'), 'Europe/Paris'))
         ->toEqual(new ConditionTreeLeaf('column', Operators::ILIKE, '%something'));
+});
+
+test('Like should be rewritten', function () {
+    $comparisons = Pattern::patternTransforms();
+
+    expect($comparisons[Operators::LIKE][0]['replacer'](new ConditionTreeLeaf('column', Operators::EQUAL, 'something'), 'Europe/Paris'))
+        ->toEqual(new ConditionTreeLeaf('column', Operators::MATCH, '/^something$/'));
+});
+
+test('ILike should be rewritten', function () {
+    $comparisons = Pattern::patternTransforms();
+
+    expect($comparisons[Operators::ILIKE][0]['replacer'](new ConditionTreeLeaf('column', Operators::EQUAL, 'something'), 'Europe/Paris'))
+        ->toEqual(new ConditionTreeLeaf('column', Operators::MATCH, '/^something$/i'));
 });
