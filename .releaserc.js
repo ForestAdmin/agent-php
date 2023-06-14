2c8601e25bd38be522595578e6eb0b9935b6006f
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-06-14 14:08:11 +0200

    chore: update monorepo github action config

--- .releaserc.js
+++ .releaserc.js
@@ -40,51 +40,51 @@ module.exports = {
       },
     ],
     "@semantic-release/github",
-    [
-      "semantic-release-slack-bot",
-      {
-        markdownReleaseNotes: true,
-        notifyOnSuccess: true,
-        notifyOnFail: false,
-        onSuccessTemplate: {
-          text: "📦 $package_name@$npm_package_version has been released!",
-          blocks: [
-            {
-              type: "section",
-              text: {
-                type: "mrkdwn",
-                text: "*New `$package_name` package released!*",
-              },
-            },
-            {
-              type: "context",
-              elements: [
-                {
-                  type: "mrkdwn",
-                  text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>",
-                },
-              ],
-            },
-            {
-              type: "divider",
-            },
-          ],
-          attachments: [
-            {
-              blocks: [
-                {
-                  type: "section",
-                  text: {
-                    type: "mrkdwn",
-                    text: "*Changes* of version $release_notes",
-                  },
-                },
-              ],
-            },
-          ],
-        },
-        packageName: "agent-php",
-      },
-    ],
+    // [
+    //   "semantic-release-slack-bot",
+    //   {
+    //     markdownReleaseNotes: true,
+    //     notifyOnSuccess: true,
+    //     notifyOnFail: false,
+    //     onSuccessTemplate: {
+    //       text: "📦 $package_name@$npm_package_version has been released!",
+    //       blocks: [
+    //         {
+    //           type: "section",
+    //           text: {
+    //             type: "mrkdwn",
+    //             text: "*New `$package_name` package released!*",
+    //           },
+    //         },
+    //         {
+    //           type: "context",
+    //           elements: [
+    //             {
+    //               type: "mrkdwn",
+    //               text: "📦  *Version:* <$repo_url/releases/tag/v$npm_package_version|$npm_package_version>",
+    //             },
+    //           ],
+    //         },
+    //         {
+    //           type: "divider",
+    //         },
+    //       ],
+    //       attachments: [
+    //         {
+    //           blocks: [
+    //             {
+    //               type: "section",
+    //               text: {
+    //                 type: "mrkdwn",
+    //                 text: "*Changes* of version $release_notes",
+    //               },
+    //             },
+    //           ],
+    //         },
+    //       ],
+    //     },
+    //     packageName: "agent-php",
+    //   },
+    // ],
   ],
 };
