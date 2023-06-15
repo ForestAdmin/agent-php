1c56100ff6124163e78d19e4945b987c87618604
Matthieu <matthv@gmail.com>
2023-06-15 15:44:21 +0200

    chore(force-release): remove Spatie Enum package (#67)
    Co-authored-by: Nicolas Alexandre <nicolasalexandre9@gmail.com>

--- packages/Agent/src/Concerns/Relation.php
+++ packages/Agent/src/Concerns/Relation.php
@@ -2,28 +2,20 @@
 
 namespace ForestAdmin\AgentPHP\Agent\Concerns;
 
-use Spatie\Enum\Enum;
-
-/**
- * @method static self ManyToMany()
- * @method static self ManyToOne()
- * @method static self OneToMany()
- * @method static self OneToOne()
- */
-final class Relation extends Enum
+final class Relation
 {
     protected static function values(): array
     {
         return [
-            'ManyToMany' => 'BelongsToMany',
-            'ManyToOne'  => 'BelongsTo',
-            'OneToMany'  => 'HasMany',
-            'OneToOne'   => 'HasOne',
+            'BelongsToMany' => 'ManyToMany',
+            'BelongsTo'     => 'ManyToOne',
+            'HasMany'       => 'OneToMany',
+            'HasOne'        => 'OneToOne',
         ];
     }
 
     public static function getRelation(string $key): string
     {
-        return array_flip(self::toArray())[$key];
+        return array_flip(self::values())[$key];
     }
 }
