6d6b678bc81926f14364ea4db164092757b81913
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2023-06-29 15:48:39 +0200

    fix(query): use relationName as alias in joins

--- packages/Agent/src/Utils/QueryBuilder.php
+++ packages/Agent/src/Utils/QueryBuilder.php
@@ -42,23 +42,25 @@ class QueryBuilder
     public function formatField(string $field): string
     {
         if (Str::contains($field, ':')) {
-            $relation = $this->collection->getFields()[Str::before($field, ':')];
+            $relationName = Str::before($field, ':');
+            $relation = $this->collection->getFields()[$relationName];
             $tableName = $this->collection
                 ->getDataSource()
                 ->getCollection($relation->getForeignCollection())->getTableName();
-            $this->addJoinRelation($relation, $tableName);
+            $this->addJoinRelation($relation, $tableName, $relationName);
 
-            return $tableName . '.' . Str::after($field, ':');
+            return $field;
         }
 
         return $this->tableName . '.' . $field;
     }
 
-    protected function addJoinRelation(RelationSchema $relation, string $relationTableName): void
+    protected function addJoinRelation(RelationSchema $relation, string $relationTableName, ?string $relationTableAlias = null): void
     {
+        $relationTableAlias = $relationTableAlias ?? $relationTableName;
         if ($relation instanceof ManyToManySchema) {
             $throughTable = $this->collection->getDataSource()->getCollection($relation->getThroughCollection())->getTableName();
-            $joinTable = "$relationTableName as $relationTableName";
+            $joinTable = "$relationTableName as $relationTableAlias";
             $joinThroughTable = "$throughTable as $throughTable";
             if (! $this->isJoin($joinTable) && ! $this->isJoin($joinThroughTable)) {
                 $this->query
@@ -69,14 +71,14 @@ class QueryBuilder
                         $throughTable . '.' . $relation->getOriginKey()
                     )
                     ->leftJoin(
-                        "$relationTableName as $relationTableName",
+                        "$relationTableName as $relationTableAlias",
                         $throughTable . '.' . $relation->getForeignKey(),
                         '=',
-                        $relationTableName . '.' . $relation->getForeignKeyTarget()
+                        $relationTableAlias . '.' . $relation->getForeignKeyTarget()
                     );
             }
         } else {
-            $joinTable = "$relationTableName as $relationTableName";
+            $joinTable = "$relationTableName as $relationTableAlias";
             if (
                 ($relation instanceof OneToOneSchema || $relation instanceof OneToManySchema)
                 && ! $this->isJoin($joinTable)
@@ -85,14 +87,14 @@ class QueryBuilder
                     $joinTable,
                     $this->tableName . '.' . $relation->getOriginKey(),
                     '=',
-                    $relationTableName . '.' . $relation->getOriginKeyTarget()
+                    $relationTableAlias . '.' . $relation->getOriginKeyTarget()
                 );
             } elseif ($relation instanceof ManyToOneSchema && ! $this->isJoin($joinTable)) {
                 $this->query->leftJoin(
                     $joinTable,
                     $this->tableName . '.' . $relation->getForeignKey(),
                     '=',
-                    $relationTableName . '.' . $relation->getForeignKeyTarget()
+                    $relationTableAlias . '.' . $relation->getForeignKeyTarget()
                 );
             }
         }
