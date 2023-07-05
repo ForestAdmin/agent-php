34c93c8b654ca6ae19a2ef5dd26cd92eb3617eca
Matt <matthv@gmail.com>
2023-07-05 15:14:13 +0200

    chore: test add relation between User & Owner

--- tests/DatasourceEloquent/Models/Owner.php
+++ tests/DatasourceEloquent/Models/Owner.php
@@ -3,6 +3,7 @@
 namespace ForestAdmin\AgentPHP\Tests\DatasourceEloquent\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Owner extends Model
@@ -10,5 +11,10 @@ class Owner extends Model
     public function cars(): BelongsToMany
     {
         return $this->belongsToMany(Car::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
--- tests/DatasourceEloquent/Models/User.php
+++ tests/DatasourceEloquent/Models/User.php
@@ -3,7 +3,12 @@
 namespace ForestAdmin\AgentPHP\Tests\DatasourceEloquent\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Model
 {
+    public function owner(): HasOne
+    {
+        return $this->hasOne(Owner::class);
+    }
 }
