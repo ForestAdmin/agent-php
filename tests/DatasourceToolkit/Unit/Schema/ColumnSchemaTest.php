15e1b0509586b9e5092dcc8f47c4168d356ea079
Matt <matthv@gmail.com>
2023-07-10 14:41:11 +0200

    refactor: column schema test

--- tests/DatasourceToolkit/Unit/Schema/ColumnSchemaTest.php
+++ tests/DatasourceToolkit/Unit/Schema/ColumnSchemaTest.php
@@ -3,62 +3,61 @@
 use ForestAdmin\AgentPHP\DatasourceToolkit\Schema\ColumnSchema;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Schema\Concerns\PrimitiveType;
 
-dataset('columnSchema', function () {
-    yield new ColumnSchema(
-        PrimitiveType::STRING,
-        [],
-        false,
-        false,
-        true,
-        'Column',
-        'foo',
-        [],
-        [],
-
-    );
+beforeEach(function () {
+    $this->bucket['columnSchema'] = new ColumnSchema(PrimitiveType::STRING, [], false, false, true, 'Column', 'foo', [], []);
 });
 
-test('getColumnType() should return columnType value', function (ColumnSchema $columnSchema) {
+test('getColumnType() should return columnType value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getColumnType())->toEqual(PrimitiveType::STRING);
-})->with('columnSchema');
+});
 
-test('getFilterOperators() should return filterOperators value', function (ColumnSchema $columnSchema) {
+test('getFilterOperators() should return filterOperators value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getFilterOperators())->toEqual([]);
-})->with('columnSchema');
+});
 
-test('isPrimaryKey() should return primaryKey value', function (ColumnSchema $columnSchema) {
+test('isPrimaryKey() should return primaryKey value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->isPrimaryKey())->toEqual(false);
-})->with('columnSchema');
+});
 
-test('isReadOnly() should return readOnly value', function (ColumnSchema $columnSchema) {
+test('isReadOnly() should return readOnly value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->isReadOnly())->toEqual(false);
-})->with('columnSchema');
+});
 
-test('isSortable() should return sortable value', function (ColumnSchema $columnSchema) {
+test('isSortable() should return sortable value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->isSortable())->toEqual(true);
-})->with('columnSchema');
+});
 
-test('setSortable() should return sortable value', function (ColumnSchema $columnSchema) {
+test('setSortable() should return sortable value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     $columnSchema->setSortable(false);
     expect($columnSchema->isSortable())->toEqual(false);
-})->with('columnSchema');
+});
 
-test('getType() should return type value', function (ColumnSchema $columnSchema) {
+test('getType() should return type value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getType())
         ->toEqual('Column');
-})->with('columnSchema');
+});
 
-test('getDefaultValueType() should return defaultValue value', function (ColumnSchema $columnSchema) {
+test('getDefaultValueType() should return defaultValue value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getDefaultValue())
         ->toEqual('foo');
-})->with('columnSchema');
+});
 
-test('getEnumValues() should return enumValues value', function (ColumnSchema $columnSchema) {
+test('getEnumValues() should return enumValues value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getEnumValues())
         ->toEqual([]);
-})->with('columnSchema');
+});
 
-test('getValidation() should return validation value', function (ColumnSchema $columnSchema) {
+test('getValidation() should return validation value', function () {
+    $columnSchema = $this->bucket['columnSchema'];
     expect($columnSchema->getValidation())
         ->toEqual([]);
-})->with('columnSchema');
+});
