e23b7673ba9e67ba406779bc437c6cc602e18eb4
Matt <matthv@gmail.com>
2024-07-25 17:32:08 +0200

    chore: add debug rename collection datasource

--- packages/DatasourceCustomizer/src/Decorators/RenameCollection/RenameCollectionDatasourceDecorator.php
+++ packages/DatasourceCustomizer/src/Decorators/RenameCollection/RenameCollectionDatasourceDecorator.php
@@ -5,6 +5,7 @@ namespace ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\RenameCollection;
 use ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\DatasourceDecorator;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Contracts\DatasourceContract;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
+use ForestAdmin\AgentPHP\DatasourceToolkit\Utils\Collection as CollectionUtils;
 use Illuminate\Support\Collection as IlluminateCollection;
 
 class RenameCollectionDatasourceDecorator extends DatasourceDecorator
@@ -66,6 +67,14 @@ class RenameCollectionDatasourceDecorator extends DatasourceDecorator
             if (isset($this->toChildName[$currentName])) {
                 throw new ForestException("Cannot rename a collection twice: {$this->toChildName[$currentName]}->{$currentName}->{$newName}");
             }
+
+            $this->getCollection($currentName)->getFields()->each(function ($fieldSchema, $fieldName) use ($currentName) {
+                if ($fieldSchema->getType() === 'PolymorphicOneToOne' || $fieldSchema->getType() === 'PolymorphicOneToMany') {
+                    $reverseRelationName = CollectionUtils::getInverseRelation($this->getCollection($currentName), $fieldName);
+
+                    throw new ForestException("Cannot rename collection $currentName because it's a target of a polymorphic relation '{$fieldSchema->getForeignCollection()}.{$reverseRelationName}'");
+                }
+            });
 
             $this->fromChildName[$currentName] = $newName;
             $this->toChildName[$newName] = $currentName;
