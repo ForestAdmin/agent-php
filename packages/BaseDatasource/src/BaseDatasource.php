2cb4cd18ba4b7d70b5e12f28f77d3207bc75c942
Matt <matthv@gmail.com>
2024-08-07 16:44:02 +0200

    feat(introspection): add new doctrine connection

--- packages/BaseDatasource/src/BaseDatasource.php
+++ packages/BaseDatasource/src/BaseDatasource.php
@@ -2,6 +2,8 @@
 
 namespace ForestAdmin\AgentPHP\BaseDatasource;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DriverManager;
 use ForestAdmin\AgentPHP\BaseDatasource\Contracts\BaseDatasourceContract;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Datasource as ForestDatasource;
 use Illuminate\Database\Capsule\Manager;
@@ -10,11 +12,14 @@ class BaseDatasource extends ForestDatasource implements BaseDatasourceContract
 {
     protected Manager $orm;
 
+    protected Connection $doctrineConnection;
+
     public function __construct(array $databaseConfig)
     {
         parent::__construct();
 
         $this->makeOrm($databaseConfig);
+        $this->makeDoctrineConnection($databaseConfig);
     }
 
     /**
@@ -25,10 +30,32 @@ class BaseDatasource extends ForestDatasource implements BaseDatasourceContract
         return $this->orm;
     }
 
+    /**
+     * @return Connection
+     */
+    public function getDoctrineConnection(): Connection
+    {
+        return $this->doctrineConnection;
+    }
+
     private function makeOrm(array $databaseConfig): void
     {
         $this->orm = new Manager();
         $this->orm->addConnection($databaseConfig);
         $this->orm->bootEloquent();
+    }
+
+    private function makeDoctrineConnection(array $databaseConfig): void
+    {
+        $config = [
+            'url'       => $databaseConfig['url'] ?? null,
+            'driver'    => $databaseConfig['driver'],
+            'user'      => $databaseConfig['username'],
+            'password'  => $databaseConfig['password'],
+            'host'      => $databaseConfig['host'],
+            'dbname'    => $databaseConfig['database'],
+        ];
+
+        $this->doctrineConnection = DriverManager::getConnection($config);
     }
 }
