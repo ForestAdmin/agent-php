618f7e32e159e90546b051b9c9d4cb0e593ae248
Matt <matthv@gmail.com>
2024-10-04 15:47:07 +0200

    fix: namespace on widgetfields

--- packages/DatasourceCustomizer/src/Decorators/Actions/WidgetField/TimePickerField.php
+++ packages/DatasourceCustomizer/src/Decorators/Actions/WidgetField/TimePickerField.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ForestAdminDatasourceCustomizer\Decorators\Action\WidgetField;
+namespace ForestAdmin\AgentPHP\ForestAdminDatasourceCustomizer\Decorators\Action\WidgetField;
 
 use ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\Actions\DynamicField;
 use ForestAdmin\AgentPHP\DatasourceCustomizer\Decorators\Actions\WidgetField\Widget;
--- packages/DatasourceCustomizer/src/Decorators/Actions/WidgetField/WidgetValidator.php
+++ packages/DatasourceCustomizer/src/Decorators/Actions/WidgetField/WidgetValidator.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ForestAdminDatasourceCustomizer\Decorators\Action\WidgetField;
+namespace ForestAdmin\AgentPHP\ForestAdminDatasourceCustomizer\Decorators\Action\WidgetField;
 
 use ForestAdmin\AgentPHP\DatasourceToolkit\Exceptions\ForestException;
 
