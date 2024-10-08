ecabdeb10e1aa9c750b8d7d4a31bc7c503ebed02
Matt <matthv@gmail.com>
2024-10-08 14:34:55 +0200

    chore: remove baseLayoutElement on root action + update rowElement

---
+++ packages/DatasourceToolkit/src/Components/Actions/Layout/RowElement.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Actions\Layout;
+
+use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Actions\ActionFieldFactory;
+
+class RowElement extends BaseLayoutElement
+{
+    public function __construct(protected array $fields)
+    {
+        parent::__construct(component: 'Row');
+        $this->fields = $this->instanciateSubfields($fields);
+    }
+
+    public function getFields(): array
+    {
+        return $this->fields;
+    }
+
+    protected function instanciateSubfields(array $fields): array
+    {
+        return array_map(static function ($field) {
+            return ActionFieldFactory::build($field->toArray());
+        }, $fields);
+    }
+}
