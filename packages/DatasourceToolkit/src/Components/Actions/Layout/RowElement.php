b17bc58e03ea9ecdbf4239b6a5bd2046ec1f60f9
Matt <matthv@gmail.com>
2024-10-10 16:55:10 +0200

    refactor: baseLayout getter/setter

--- packages/DatasourceToolkit/src/Components/Actions/Layout/RowElement.php
+++ packages/DatasourceToolkit/src/Components/Actions/Layout/RowElement.php
@@ -18,19 +18,4 @@ class RowElement extends BaseLayoutElement
     {
         $this->fields = $fields;
     }
-
-    public function __set($key, $value)
-    {
-        $this->$key = $value;
-    }
-
-    public function __get($key)
-    {
-        return $this->$key;
-    }
-
-    public function __isset($key)
-    {
-        return isset($this->$key);
-    }
 }
