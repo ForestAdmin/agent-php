088ae3779a2e8b3af4514bcf2ee61518554b3f10
Matt <matthv@gmail.com>
2024-10-10 09:23:49 +0200

    fix: class RowElement toolkit

--- packages/DatasourceToolkit/src/Components/Actions/Layout/RowElement.php
+++ packages/DatasourceToolkit/src/Components/Actions/Layout/RowElement.php
@@ -2,14 +2,11 @@
 
 namespace ForestAdmin\AgentPHP\DatasourceToolkit\Components\Actions\Layout;
 
-use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Actions\ActionFieldFactory;
-
 class RowElement extends BaseLayoutElement
 {
     public function __construct(protected array $fields)
     {
         parent::__construct(component: 'Row');
-        $this->fields = $this->instanciateSubfields($fields);
     }
 
     public function getFields(): array
@@ -17,10 +14,8 @@ class RowElement extends BaseLayoutElement
         return $this->fields;
     }
 
-    protected function instanciateSubfields(array $fields): array
+    public function setFields(array $fields): void
     {
-        return array_map(static function ($field) {
-            return ActionFieldFactory::build($field); // to check
-        }, $fields);
+        $this->fields = $fields;
     }
 }
