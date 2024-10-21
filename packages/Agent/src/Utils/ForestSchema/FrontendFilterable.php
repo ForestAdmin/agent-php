565744a0c7670dee5f9251cd97fcf7e33afbf23e
Matt <matthv@gmail.com>
2024-10-21 15:40:56 +0200

    fix: frontendFilterable

--- packages/Agent/src/Utils/ForestSchema/FrontendFilterable.php
+++ packages/Agent/src/Utils/ForestSchema/FrontendFilterable.php
@@ -4,7 +4,6 @@ namespace ForestAdmin\AgentPHP\Agent\Utils\ForestSchema;
 
 use ForestAdmin\AgentPHP\DatasourceToolkit\Components\Query\ConditionTree\Operators;
 use ForestAdmin\AgentPHP\DatasourceToolkit\Schema\Concerns\PrimitiveType;
-use Illuminate\Support\Collection as IlluminateCollection;
 
 final class FrontendFilterable
 {
@@ -58,14 +57,7 @@ final class FrontendFilterable
         'Json'     => [],
     ];
 
-    //    public static function isFilterable(string|array $type, array $operators = []): bool
-    //    {
-    //        $neededOperators = new IlluminateCollection(self::getRequiredOperators($type));
-    //        $supportedOperators = new IlluminateCollection($operators);
-    //
-    //        return $neededOperators->isNotEmpty() && $neededOperators->every(fn ($operator) => $supportedOperators->contains($operator));
-    //    }
-    public static function isFilterable($operator): bool
+    public static function isFilterable(array $operator): bool
     {
         return $operator && ! empty($operator);
     }
